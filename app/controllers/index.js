32b33651c337bacd0332f2d7bed4b7d14ff3c0ba
claus-expoze <claus@multimedion.dk>
2015-10-19 11:48:13 +0200

    Startup on frontpage with login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,16 @@
-function doClick(e) {
-    alert($.label.text);
+function doTest1(e) {
+	
+	/*
+	 * If we have userid in properties fire test1
+	 * else open login
+	 */
+	if(Ti.App.Properties.getString('userid')){
+		test1win = Alloy.createController('test-1');
+	}else{
+		loginwin = Alloy.createController('login');
+	}
+	
+    //alert('testing');
 }
 
 $.index.open();
